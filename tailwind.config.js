c3ca9d5a07fca6bc1f213cc43d64ef152f2cc77b
John <jovanpolovina@gmail.com>
2025-01-01 21:35:01 +0100

    Ažuriranje package.json i tailwind konfiguracije

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Paths to your source files
   theme: {
     extend: {
