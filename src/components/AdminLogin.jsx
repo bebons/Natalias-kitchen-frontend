006b1327627895d67f0f9b062dcda2b13d744a5b
John <jovanpolovina@gmail.com>
2025-01-02 12:23:48 +0100

    Update foodApi.js and resolve issues with baseURL

--- src/components/AdminLogin.jsx
+++ src/components/AdminLogin.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import axios from "axios";
-import getBaseUrl from "../utils/baseURL";
+import getBaseUrl from "../utils/baseUrl";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 
