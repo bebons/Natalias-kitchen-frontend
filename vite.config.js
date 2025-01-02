390845de1ca132e3509ca9c7f3bb4980e7d6f178
John <jovanpolovina@gmail.com>
2025-01-02 11:40:31 +0100

    Fix ordersApi path issue and update Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // Alias za jednostavnije uvođenje putanja
+    },
+  },
   build: {
     outDir: "dist",
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ["react", "react-dom", "react-router-dom", "axios"], // Odvojeni chunk-ovi za veće biblioteke
+          vendor: ["react", "react-dom", "react-router-dom", "axios"],
         },
       },
     },
